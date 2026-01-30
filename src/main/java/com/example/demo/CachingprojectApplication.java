d5eb481fa874f3e54d2c3c3d9f46f1987b3e9e77
Arthur Budny <budnyarthur@gmail.com>
2026-01-30 14:54:59 -0300

    init commit

---
+++ src/main/java/com/example/demo/CachingprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingprojectApplication.class, args);
+	}
+
+}
